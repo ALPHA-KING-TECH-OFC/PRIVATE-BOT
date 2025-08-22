74495ef31aa210e997a23917963cc0d690ecc2fa
Developer <queenalphatech123@gmail.com>
2025-08-22 21:31:05 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
+
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
+}
+
+module.exports = {
+  SESSION_ID: process.env.SESSION_ID || "", //enter your session ld here pal
+  OWNER_NUM: process.env.OWNER_NUM || "923211519025", // don't change the owner number or the bot will have many errors
+  PREFIX: process.env.PREFIX || ".",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
+  MODE : process.env.MODE || "public", 
+  AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "true", 
+  AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING || "true"), 
+  ANTI_DELETE: convertToBool(process.env.ANTI_DELETE || "true"),
+};
+// THIS WAS MADE BY SIGMA TECH
+// DONT CLONE MY CODES
